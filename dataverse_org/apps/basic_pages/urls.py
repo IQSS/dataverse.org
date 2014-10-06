b58fadcf823726ce49d0960f596c5edbbcbd48e2
Raman Prasad <raman_prasad@harvard.edu>
2014-10-06 14:58:36 -0400

    change home url to "/"

--- dataverse_org/apps/basic_pages/urls.py
+++ dataverse_org/apps/basic_pages/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.basic_pages.views',
 
-    url(r'^home/$', 'view_homepage', name="view_homepage"),
+    #url(r'^home/$', 'view_homepage', name="view_homepage"),
 
     url(r'^support/$', 'view_support_page', name="view_support_page"),
 
     url(r'^best-practices/$', 'view_best_practices_page', name="view_best_practices_page"),
 
-    url(r'^/?$', 'view_homepage', name="default_homepage"),
+    #url(r'^/?$', 'view_homepage', name="default_homepage"),
+    url(r'^/?$', 'view_homepage', name="view_homepage"),
 
 
 )
