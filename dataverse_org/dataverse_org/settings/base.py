c6bcd5fa86596f9ec1ff454045ca3c088bfc54cd
Raman Prasad <raman_prasad@harvard.edu>
2014-12-11 11:18:26 -0500

    remove append slash

--- dataverse_org/dataverse_org/settings/base.py
+++ dataverse_org/dataverse_org/settings/base.py
@@ -170,9 +170,6 @@ MIDDLEWARE_CLASSES = (
 ########## END MIDDLEWARE CONFIGURATION
 
 
-# Remove append slash - initially done for flatpages
-APPEND_SLASH = False
-
 ########## URL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
 ROOT_URLCONF = '%s.urls' % SITE_NAME
