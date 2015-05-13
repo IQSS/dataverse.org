5ed3fea1846e9a2fd07efb842a5a80ba929f25d1
Michael Heppler <mheppler@hmdc.harvard.edu>
2015-05-13 17:04:19 -0400

    Fixed broken image URLs for footer and favicon. Updated copyright year.

--- docs/community/source/conf.py
+++ docs/community/source/conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Dataverse Community'
-copyright = u'2014, The President & Fellows of Harvard College '
+copyright = u'2015, The President & Fellows of Harvard College '
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
